33caa7f37f521d68697d9f04a2055240eec91d9c
Guilherme <gjardimduarte@gmail.com>
2017-03-04 14:42:46 -0300

    add cam_listaProposicoes

---
+++ R/cam_listaProposicoes.R
@@ -0,0 +1,37 @@
+#' @import xml2
+#' @import util
+#' @import lubridate
+#' @import magrittr
+#' @title Downloads and tidies data on the agenda in the Federal Senate.
+#' @param initial_data (\code{character}) start date of the period requested.
+#' This parameter must be in the format YYYYMMDD (Year-Month-Day). A value for
+#' this parameter is necessary, all others are optional.
+#' @param end_data (\code{character}) final date for period requested. Format
+#' YYYYMMDD.
+#' @param house (\code{character}). The acronym for the legislative house
+#' for which results are requested. Options are SF (\emph{Senado Federal}, Federal Senate) and CN (\emph{Congresso Nacional}, National Congress - joint meeting of the Senate and Chamber).
+#' @param colegiado To Do
+#' @param legislator To Do
+#' @param details (\code{logical}). If details is equal to TRUE, the data returned
+#' is an expanded dataset with additional details. This is not recommended unless
+#' necessary.
+#' @return A tibble, of classes \code{tbl_df}, \code{tbl} and \code{data.frame}.
+#' @note Requesting data from a long period of time with \code{details = TRUE} will
+#' return a large object in terms of memory. It will also be rather unwieldy, with
+#' many columns.
+#' @author Robert Myles McDonnell & Guilherme Jardim Duarte.
+#' @examples
+#' sen_agenda(initial_date = "20161105", end_date = "20161125")
+cam_listaProposicoes <- function(sigla) {
+
+  link <- "http://www.camara.leg.br/SitCamaraWS/Proposicoes.asmx/ListarProposicoes?" %p%
+    "sigla=" %>% sigla %>%
+    "&numero=" %>% numero %>%
+    "&ano=2011&datApresentacaoIni=14/11/2011&datApresentacaoFim=16/11/2011&parteNomeAutor=&idTipoAutor=&siglaPartidoAutor=&siglaUFAutor=&generoAutor=&codEstado=&codOrgaoEstado=&emTramitacao="
+
+  return(link)
+}
+
+
+
+
