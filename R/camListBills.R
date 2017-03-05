2a8ba6c960befc736579659b7d3df3580e455690
Danilo Freire <danilofreire@gmail.com>
2017-03-05 16:27:43 +0000

    add danilo as author

--- R/camListBills.R
+++ R/camListBills.R
@@ -20,7 +20,7 @@
 #' @note Requesting data from a long period of time with \code{details = TRUE} will
 #' return a large object in terms of memory. It will also be rather unwieldy, with
 #' many columns.
-#' @author Robert Myles McDonnell & Guilherme Jardim Duarte.
+#' @author Robert Myles McDonnell, Guilherme Jardim Duarte & Danilo Freire.
 #' @examples
 #' sen_agenda(initial_date = "20161105", end_date = "20161125")
 cam_listaProposicoes <- function(sigla) {
