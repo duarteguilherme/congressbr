9e8ca84715c688b764388d11f863e558338196a4
RobertMyles <mcdonnell.robert5@gmail.com>
2017-03-13 20:52:30 -0300

    feat: bill status

--- R/sen_bills_various.R
+++ R/sen_bills_various.R
@@ -49,13 +49,6 @@ sen_bills_subtypes <- function(active = NULL, ascii = TRUE){
 
 
 
-#' @importFrom httr GET
-#' @importFrom httr content
-#' @importFrom purrr map_chr
-#' @importFrom dplyr mutate
-#' @importFrom dplyr data_frame
-#' @importFrom stringi stri_trans_general
-#' @importFrom lubridate parse_date_time
 #' @title Downloads and tidies information on the types of deadline and time
 #' limits for legislation in the Federal Senate.
 #' @param ascii \code{logical}. If \code{TRUE}, strips Latin characters from
@@ -86,14 +79,6 @@ sen_bills_limits <- function(ascii = TRUE){
 
 
 
-
-#' @importFrom httr GET
-#' @importFrom httr content
-#' @importFrom purrr map_chr
-#' @importFrom dplyr mutate
-#' @importFrom dplyr data_frame
-#' @importFrom stringi stri_trans_general
-#' @importFrom lubridate parse_date_time
 #' @title Downloads and tidies information on the topics of legislation in
 #' the Federal Senate.
 #' @param active \code{character}. Options are "Yes", "No" or \code{NULL}, the
@@ -137,13 +122,6 @@ sen_bills_topics <- function(active = NULL, ascii = TRUE){
 
 
 
-#' @importFrom httr GET
-#' @importFrom httr content
-#' @importFrom purrr map_chr
-#' @importFrom dplyr mutate
-#' @importFrom dplyr data_frame
-#' @importFrom stringi stri_trans_general
-#' @importFrom lubridate parse_date_time
 #' @title Downloads and tidies information on legislation that is under
 #' consideration in the Federal Senate.
 #' @param year Format YYYY. Returns legislation from this year.
@@ -210,13 +188,6 @@ sen_bills_passing <- function(year = NULL,  number = NULL,
 
 
 
-#' @importFrom httr GET
-#' @importFrom httr content
-#' @importFrom purrr map_chr
-#' @importFrom dplyr mutate
-#' @importFrom dplyr data_frame
-#' @importFrom stringi stri_trans_general
-#' @importFrom lubridate parse_date_time
 #' @title Downloads and tidies information on legislation from the current
 #' legislature of the Federal Senate.
 #' @param year Format YYYY. Returns legislation from this year.
@@ -285,3 +256,73 @@ sen_bills_current <- function(year = NULL, date = NULL,
   )
   return(bills)
 }
+
+
+
+
+
+#' @title Downloads and tidies information on legislation from the current
+#' legislature of the Federal Senate.
+#' @param id \code{integer}. This number is the id given to each bill in the
+#' Senate database. For example, running \code{sen_bills_current()} will return a
+#'  dataframe with the variable \code{bill_id} in the first column. These numbers
+#'  can be used as this id.
+#' @param ascii \code{logical}. If \code{TRUE}, strips Latin characters from
+#' strings.
+#' @return A tibble, of classes \code{tbl_df}, \code{tbl} and \code{data.frame}.
+#' @author Robert Myles McDonnell, Guilherme Jardim Duarte & Danilo Freire.
+#' @examples
+#' sen_bills_status(id = 80406)
+#' @export
+sen_bills_status <- function(id = NULL, ascii = TRUE){
+
+  if(is.null(id)){
+    stop("Please enter a value for 'id'.")
+  }
+  base_url <- "http://legis.senado.gov.br/dadosabertos/materia/situacaoatual/" %p%
+    id
+
+  request <- httr::GET(base_url)
+  request <- status(request)
+  request <- request$SituacaoAtualMateria$Materias
+  Null <- NA_character_
+  req <- request$Materia$Autuacoes
+
+  stat <- dplyr::data_frame(
+    bill_id = purrr::map_chr(request, "Codigo", .null = Null),
+    bill_type = purrr::map_chr(request, "Subtipo", .null = Null),
+    bill_number = purrr::map_chr(request, "Numero", .null = Null),
+    bill_year = purrr::map_chr(request, "Ano", .null = Null),
+    bill_details = purrr::map_chr(request, "Ementa", .null = Null),
+    bill_in_passage = purrr::map_chr(request, "Tramitando", .null = Null),
+    bill_situation = purrr::map_chr(req, "DescricaoSituacao", .null = Null),
+    situation_date = purrr::map_chr(req, "DataSituacao", .null = Null),
+    situation_place = purrr::map_chr(req, "NomeLocal", .null = Null)
+  )
+
+  stat <- stat %>%
+    dplyr::mutate(situation_date = lubridate::parse_date_time(
+      situation_date, orders = "Ymd"),
+     bill_in_passage = dplyr::case_when(
+        grepl("Nao|Não", stat$bill_in_passage) ~ "No",
+        grepl("Sim", stat$bill_in_passage) ~ "Yes",
+        TRUE ~ "Not recorded")
+     )
+
+  if(ascii == TRUE){
+    stat <- stat %>%
+      dplyr::mutate(
+        bill_details = stringi::stri_trans_general(bill_details,
+                                                   "Latin-ASCII"),
+        bill_situation = stringi::stri_trans_general(bill_situation,
+                                                     "Latin-ASCII"),
+        situation_place = stringi::stri_trans_general(situation_place,
+                                                      "Latin-ASCII")
+      )
+  }
+  return(stat)
+}
+
+
+
+
