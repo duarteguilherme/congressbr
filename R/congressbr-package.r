02e3397834b1d25d3c1429739d429fdf090f30b1
RobertMyles <mcdonnell.robert5@gmail.com>
2017-04-22 15:45:46 -0300

    chore: fixes for R CMD

--- R/congressbr-package.r
+++ R/congressbr-package.r
@@ -1,5 +1,7 @@
 #' congressbr.
 #'
 #' @name congressbr
+#' @description A package for obtaining data from the APIs of the Brazilian
+#' Chamber of Deputies and the Federal Senate, respectively.
 #' @docType package
 NULL
--- R/sen_bills.R
+++ R/sen_bills.R
@@ -30,7 +30,7 @@
 #' pls_5_2010 <- sen_bills(type = "PLS", number = 5, year = 2010)
 #'
 #' # Get info on the first bill in the dataframe returned
-#' # by \code{sen_bills_current()}, which has an id of 25:
+#' # by sen_bills_current(), which has an id of 25:
 #' sen25 <- sen_bills(bill_id = 25)
 #' @export
 sen_bills <- function(bill_id = NULL, type = NULL,
